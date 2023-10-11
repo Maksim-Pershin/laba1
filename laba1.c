229ee1b43a00adfc814d78381f3ffd21f6e16f1b
Maksim-Pershin <147662806+Maksim-Pershin@users.noreply.github.com>
2023-10-11 23:44:11 +0300

    Add files via upload

---
+++ laba1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+float u (float y, float x, float z)
+{
+float w=-sqrt(y*y+((4*x*x)/3));
+float g=(5*cos(x*y)*cos(x*y)*cos(x*y)*cos(x*y))/(x*z);
+}
+int main()
+{
+ for(float x=1;x<=3;x+=1.3)
+ {
+ for(float y=2;y<=4;y+=1.3)
+ {
+ for(float z=0.5;z<=1.5;z+=0.8)
+ {
+ printf("y=%f x=%f z=%f u(y,x,z)=%f\n",y,x,z,u(y,x,z));
+ }
+ } 
+ }
+ return 0;
+}
